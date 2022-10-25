8c9e64a5052f32ef0e43c060b146ffb1fa58a719
shad mustafa <shadmanmi2018@gmail.com>
2022-10-25 23:01:00 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main=document.getElementById('main').remove();
+const newHeader = document.createElement("h1");
+// const newHeader=document.querySelector('h1');
+newHeader.id='victory';
+newHeader.innerHTML="is the champion";
+document.body.appendChild(newHeader);
